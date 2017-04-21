2212c5b5462e814b48daa15649f3bfbf7b7fdaed
HabibAroua <habibha.aroua82@gmail.com>
2017-04-21 23:24:45 +0200

    Participer

--- WebService/app/src/main/java/habib/webservice/Login.java
+++ WebService/app/src/main/java/habib/webservice/Login.java
@@ -8,12 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
