3d88dd6db05e3989fe854f2bcfba42e2b0e02e50
Habib Aroua <habib.aroua@sesame.com.tn>
2019-10-04 21:40:32 +0100

    Update InformationAdherent.java

--- WebService/app/src/main/java/habib/webservice/Activity/InformationAdherent.java
+++ WebService/app/src/main/java/habib/webservice/Activity/InformationAdherent.java
@@ -2,16 +2,15 @@ package habib.webservice.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import habib.webservice.R;
 
-public class InformationAdherent extends AppCompatActivity {
-
+public class InformationAdherent extends AppCompatActivity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_adherent);
         getSupportFragmentManager().beginTransaction().add(R.id.infoAdherent, new habib.webservice.Fragemnts.ModifierInfoPers()).commit();
-
     }
 }
