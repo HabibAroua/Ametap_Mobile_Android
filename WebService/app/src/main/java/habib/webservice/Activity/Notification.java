1b0da6e67d9ebce8b1a48e8f3a3237bcbc3ada4a
Habib Aroua <habib.aroua@sesame.com.tn>
2019-10-04 21:38:38 +0100

    Update Notification.java

--- WebService/app/src/main/java/habib/webservice/Activity/Notification.java
+++ WebService/app/src/main/java/habib/webservice/Activity/Notification.java
@@ -2,13 +2,13 @@ package habib.webservice.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import habib.webservice.R;
 
-public class Notification extends AppCompatActivity {
-
+public class Notification extends AppCompatActivity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
         getSupportFragmentManager().beginTransaction().add(R.id.Act_notif, new habib.webservice.Fragemnts.AfficheNotif()).commit();
