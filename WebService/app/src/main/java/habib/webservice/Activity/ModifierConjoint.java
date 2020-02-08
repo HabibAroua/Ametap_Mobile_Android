181088fed4b8e8ac326cf3f558594e69808f6489
Habib Aroua <habib.aroua@sesame.com.tn>
2020-02-09 00:07:54 +0100

    Update ModifierConjoint.java

--- WebService/app/src/main/java/habib/webservice/Activity/ModifierConjoint.java
+++ WebService/app/src/main/java/habib/webservice/Activity/ModifierConjoint.java
@@ -2,7 +2,6 @@ package habib.webservice.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import habib.webservice.R;
 
 public class ModifierConjoint extends AppCompatActivity
@@ -14,7 +13,5 @@ public class ModifierConjoint extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modifier_conjoint);
         getSupportFragmentManager().beginTransaction().add(R.id.activityModifierConjoint, new habib.webservice.Fragemnts.ModifierConjoint()).commit();
-
-
     }
 }
