341a49c24e43999d14893283a98380a3a38dc48b
Habib Aroua <habib.aroua@sesame.com.tn>
2020-02-09 00:03:04 +0100

    Update AjouterConjoint.java

--- WebService/app/src/main/java/habib/webservice/Activity/AjouterConjoint.java
+++ WebService/app/src/main/java/habib/webservice/Activity/AjouterConjoint.java
@@ -2,16 +2,15 @@ package habib.webservice.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import habib.webservice.R;
 
-public class AjouterConjoint extends AppCompatActivity {
-
+public class AjouterConjoint extends AppCompatActivity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_conjoint);
         getSupportFragmentManager().beginTransaction().add(R.id.ajouterConjoint, new habib.webservice.Fragemnts.Ajouter_Conjoint()).commit();
-
     }
 }
