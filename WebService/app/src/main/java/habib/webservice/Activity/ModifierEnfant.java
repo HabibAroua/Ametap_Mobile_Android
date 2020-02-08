0b4cba31f556b0c43cc43991b0b12c8f19bc4085
Habib Aroua <habib.aroua@sesame.com.tn>
2020-02-09 00:08:33 +0100

    Update ModifierEnfant.java

--- WebService/app/src/main/java/habib/webservice/Activity/ModifierEnfant.java
+++ WebService/app/src/main/java/habib/webservice/Activity/ModifierEnfant.java
@@ -2,17 +2,15 @@ package habib.webservice.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import habib.webservice.R;
 
-public class ModifierEnfant extends AppCompatActivity {
-
+public class ModifierEnfant extends AppCompatActivity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
-        //getSupportFragmentManager().beginTransaction().add(R.id.activityModifierConjoint, new habib.webservice.Fragemnts.ModifierConjoint()).commit();
         setContentView(R.layout.activity_modifier_enfant);
         getSupportFragmentManager().beginTransaction().add(R.id.activityModifierEnfant, new habib.webservice.Fragemnts.ModifierEnfant()).commit();
-
     }
 }
