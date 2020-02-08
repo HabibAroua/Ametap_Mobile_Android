d2edc1b8efaeaeb0272dc12c39a2e0426ab6248e
Habib Aroua <habib.aroua@sesame.com.tn>
2020-02-09 00:04:27 +0100

    Update HistoriqueActivite.java

--- WebService/app/src/main/java/habib/webservice/Activity/HistoriqueActivite.java
+++ WebService/app/src/main/java/habib/webservice/Activity/HistoriqueActivite.java
@@ -2,16 +2,15 @@ package habib.webservice.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import habib.webservice.R;
 
-public class HistoriqueActivite extends AppCompatActivity {
-
+public class HistoriqueActivite extends AppCompatActivity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historique_activite);
         getSupportFragmentManager().beginTransaction().add(R.id.histAct, new habib.webservice.Fragemnts.Historique_des_activites()).commit();
-
     }
 }
