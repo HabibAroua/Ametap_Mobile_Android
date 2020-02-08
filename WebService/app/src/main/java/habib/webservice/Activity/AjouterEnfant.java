4d65ffb51a9604fec15b2c995ca0b8f88438c607
Habib Aroua <habib.aroua@sesame.com.tn>
2020-02-09 00:03:37 +0100

    Update AjouterEnfant.java

--- WebService/app/src/main/java/habib/webservice/Activity/AjouterEnfant.java
+++ WebService/app/src/main/java/habib/webservice/Activity/AjouterEnfant.java
@@ -2,13 +2,13 @@ package habib.webservice.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import habib.webservice.R;
 
-public class AjouterEnfant extends AppCompatActivity {
-
+public class AjouterEnfant extends AppCompatActivity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_enfant);
         getSupportFragmentManager().beginTransaction().add(R.id.ajouterEnfant, new habib.webservice.Fragemnts.ajouter_Enfant()).commit();
